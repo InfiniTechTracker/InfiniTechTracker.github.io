20319d1278bdbb4c33e37a29ff57a6eea9400124
Josh S <joshuasalinas@tamu.edu>
2021-12-05 21:00:38 -0600

    Update nav bar and build.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,8 @@ import { ContactComponent } from './contact/contact.component';
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    MatDividerModule
+    MatDividerModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
